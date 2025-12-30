2ac9d69d35daa95e46ffe61a9cb8bcd27b6c1337
Michi Schwarz <michischwarz@posteo.de>
2025-12-30 11:10:45 +0100

    feat: Add API for random number generation feature and update HTML structure

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
-console.log("Die Seite wurde geladen!");
-alert("Willkommen auf meiner Website!");
+window.onload = () => {
+    console.log("Die Seite wurde geladen!");
+    alert("Willkommen auf meiner Website!");
 
-document.getElementById("button001").addEventListener("click", () => {
-    alert("Du hast geklickt!");
-});
+    document.getElementById("button001").addEventListener("click", () => {
+        alert("Du hast geklickt!");
+    });
 
-document.getElementById("button003_dyn_text").addEventListener("click", () => {
-    document.getElementById("dynText").innerText = "Der Text wurde geändert!";
-});
+    document.getElementById("button003_dyn_text").addEventListener("click", () => {
+        document.getElementById("dynText").innerText = "Der Text wurde geändert!";
+    });
 
-document.getElementById("button004_toggle_secret").addEventListener("click", () => {
-    const text = document.getElementById("secretText"); 
-    text.classList.toggle("hidden");
-});
+    document.getElementById("button004_toggle_secret").addEventListener("click", () => {
+        const text = document.getElementById("secretText"); 
+        text.classList.toggle("hidden");
+    });
 
 
-let count_var = 0;
+    let count_var = 0;
 
-document.getElementById("plusButton").addEventListener("click", () => {
-    count_var++;
-    document.getElementById("counter").textContent = count_var;
-});
+    document.getElementById("plusButton").addEventListener("click", () => {
+        count_var++;
+        document.getElementById("counter").textContent = count_var;
+    });
 
-document.getElementById("minusButton").addEventListener("click", () => {
-    count_var--;
-    document.getElementById("counter").textContent = count_var;
-});
+    document.getElementById("minusButton").addEventListener("click", () => {
+        count_var--;
+        document.getElementById("counter").textContent = count_var;
+    });
+
+    document.getElementById("fetch_rand").addEventListener("click", () => {
+        fetch("http://127.0.0.1:5000/api/random")
+            .then(res => res.json())
+            .then(data => {
+                console.log("DATA VOM SERVER:", data); 
+                console.log("TYP:", typeof data.number);
+                document.getElementById("rand_num").textContent = data.number;
+            });
+    });
+};
---
+++ server.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+import random
+
+app = Flask(__name__)
+CORS(app, origins=["http://127.0.0.1:5500"])
+
+@app.get("/api/random")
+def random_number():
+    number = random.randint(0, 100)
+    return jsonify({"number": number})
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
