0d61c624671a7742e4aeff3ed848ccb3d4414c3e
Michi Schwarz <michischwarz@posteo.de>
2025-12-30 11:57:33 +0100

    fix: Update CORS settings to allow requests from the production URL

--- backend/server.py
+++ backend/server.py
@@ -3,7 +3,10 @@ from flask_cors import CORS
 import random
 
 app = Flask(__name__)
-CORS(app, origins=["http://127.0.0.1:5500"])
+CORS(app, origins=[
+    "http://127.0.0.1:5500",
+    "https://michischwarz.github.io"
+])
 
 @app.get("/") 
 def home(): 
